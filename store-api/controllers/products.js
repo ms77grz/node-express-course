3bb7ad7ea56c9aa2e932427a57f6dc9ca40038f9
ms77grz <ms77grz@gmail.com>
2021-10-15 17:38:05 +0300

    Setup Pagination Using Skip and Limit

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,9 +1,11 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    name: { $regex: 'aB', $options: 'i' },
-  });
+  const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(4)
+    .skip(1);
   if (!products.length) {
     throw new Error('No products here.');
   }
@@ -11,7 +13,8 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
+  // filter
   const query = {};
   if (featured) {
     query.featured = featured === 'true' ? true : false;
@@ -22,7 +25,25 @@ const getAllProducts = async (req, res) => {
   if (name) {
     query.name = { $regex: name, $options: 'i' };
   }
-  const products = await Product.find(query);
+  let result = Product.find(query);
+  // sort
+  if (sort) {
+    const sortList = sort.replace(/,/g, ' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+  // select certain fields
+  if (fields) {
+    const fieldsList = fields.replace(/,/g, ' ');
+    result = result.select(fieldsList);
+  }
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+  const products = await result;
   if (!products.length) {
     throw new Error('No products here.');
   }
