649f92970822f2c85b86995210d4f7f232bc97e5
ms77grz <ms77grz@gmail.com>
2021-10-21 12:20:27 +0300

    Cast Error / StatusCodes

--- jobs-api/middleware/error-handler.js
+++ jobs-api/middleware/error-handler.js
@@ -1,4 +1,3 @@
-const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
@@ -6,14 +5,24 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong, try again later',
   };
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
-  }
+  // duplicate error
   if (err.code && err.code === 11000) {
-    customError.statusCode = 400;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please choose another value`;
+  }
+  // validation error
+  if (err.name && err.name === 'ValidationError') {
+    (customError.statusCode = StatusCodes.BAD_REQUEST),
+      (customError.msg = Object.values(err.errors).map(
+        item => `${item.message} for ${item.path} field`
+      ));
+  }
+  // cast error
+  if (err.name && err.name === 'CastError') {
+    (customError.statusCode = StatusCodes.NOT_FOUND),
+      (customError.msg = `No job with id ${err.value}`);
   }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err });
   return res.status(customError.statusCode).json({ msg: customError.msg });
