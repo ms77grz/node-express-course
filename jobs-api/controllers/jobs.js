9c451b4ab20ab3e2ba5430a07ea561c6684af278
ms77grz <ms77grz@gmail.com>
2021-10-21 09:49:40 +0300

    deleteJob functionality

--- jobs-api/controllers/jobs.js
+++ jobs-api/controllers/jobs.js
@@ -51,7 +51,18 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send('delete job');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findOneAndDelete({
+    _id: jobId,
+    createdBy: userId,
+  });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
