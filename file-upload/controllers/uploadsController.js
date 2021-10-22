4eb794b5409fbf84abc8339253473f165eb0af54
ms77grz <ms77grz@gmail.com>
2021-10-22 17:44:00 +0300

    Complete File Upload

---
+++ file-upload/controllers/uploadsController.js
@@ -0,0 +1,19 @@
+const { StatusCodes } = require('http-status-codes');
+const path = require('path');
+
+const uploadProductImage = async (req, res) => {
+  const productImage = req.files.image;
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/',
+    productImage.name
+  );
+  await productImage.mv(imagePath);
+  res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+module.exports = {
+  uploadProductImage,
+};
