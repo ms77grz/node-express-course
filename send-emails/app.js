d149738f860be0161bf605566d66fbea1e387c15
Magomed Shamaev <ms77grz@gmail.com>
2021-10-23 22:49:35 +0300

    Send Emails with "SendGrid" Normal Email Service

--- send-emails/app.js
+++ send-emails/app.js
@@ -4,7 +4,7 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
-const sendEmail = require('./controllers/sendEmail');
+const { sendEmailEthereal, sendEmail } = require('./controllers/sendEmail');
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
@@ -12,11 +12,18 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 
+const html = `
+<h1>Email Project</h1>
+<a href="/send-ethereal">send email to Ethereal</a><br>
+<a href="/send-smtp">send email to real mail server</a>
+`;
+
 // routes
 app.get('/', (req, res) => {
-  res.send('<h1>Email Project</h1><a href="/send">send email</a>');
+  res.send(html);
 });
-app.get('/send', sendEmail);
+app.get('/send-ethereal', sendEmailEthereal);
+app.get('/send-smtp', sendEmail);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
--- send-emails/controllers/sendEmail.js
+++ send-emails/controllers/sendEmail.js
@@ -1,5 +1,45 @@
-const sendEmail = async (req, res) => {
-  res.send('send email func');
+const nodemailer = require('nodemailer');
+
+const sendEmailEthereal = async (req, res) => {
+  const testAccount = await nodemailer.createTestAccount();
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'tressa.jerde@ethereal.email',
+      pass: '7uSVqEVcWFfrQraSpv',
+    },
+  });
+  const message = {
+    from: '"Webmshop" <ms77grz@gmail.com>',
+    to: 'bar@example.com, baz@example.com',
+    subject: 'Testing functionality 2',
+    html: '<h2>Sending Emails With NodeJS</h2>',
+  };
+  const info = await transporter.sendMail(message);
+  res.json(info);
 };
 
-module.exports = sendEmail;
+const sendEmail = async (req, res) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  const message = {
+    from: `"Webmshop"<${process.env.EMAIL_FROM}>`,
+    to: process.env.EMAIL_TO,
+    subject: 'Testing functionality',
+    html: '<h2>Sending Emails With NodeJS To VT</h2>',
+  };
+  const info = await transporter.sendMail(message);
+  res.json(info);
+};
+
+module.exports = {
+  sendEmailEthereal,
+  sendEmail,
+};
