140ec6e4ad0a068a8b9d4fdeea3a8090cd5c987b
Magomed Shamaev <ms77grz@gmail.com>
2021-10-23 13:12:35 +0300

    Remove Temp Files

--- file-upload/controllers/uploadsController.js
+++ file-upload/controllers/uploadsController.js
@@ -1,8 +1,37 @@
 const { StatusCodes } = require('http-status-codes');
 const path = require('path');
+const CustomError = require('../errors');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
 const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: 'file-upload',
+    }
+  );
+  fs.unlinkSync(req.files.image.tempFilePath);
+  res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
+};
+
+const uploadProductImageLocal = async (req, res) => {
+  // check if file exists
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded');
+  }
   const productImage = req.files.image;
+  // check format
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image');
+  }
+  // check size
+  if (productImage.size > process.env.IMAGE_MAX_SIZE) {
+    throw new CustomError.BadRequestError(
+      `Image Size Must Be Less Then ${process.env.IMAGE_MAX_SIZE / 1000} KB`
+    );
+  }
   const imagePath = path.join(
     __dirname,
     '../public/uploads/',
