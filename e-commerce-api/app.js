ab16f95971ae693b52635ff1e7d10eff4f1d77de
ms77grz <ms77grz@gmail.com>
2021-10-25 09:16:10 +0300

    E-Commerce API - Initial Setup

---
+++ e-commerce-api/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    app.listen(port, console.log(`Server is listening on port ${port}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
