d7603704041d0c5ae957df5931a2357a89b34a7a
ms77grz <ms77grz@gmail.com>
2021-10-25 17:46:29 +0300

    User Roles

--- e-commerce-api/controllers/authController.js
+++ e-commerce-api/controllers/authController.js
@@ -1,5 +1,18 @@
+const { StatusCodes } = require('http-status-codes');
+const User = require('../models/User');
+const CustomError = require('../errors');
+
 const register = async (req, res) => {
-  res.send('register user');
+  const { name, email, password } = req.body;
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError('Email already exists');
+  }
+  // the first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? 'admin' : 'user';
+  const user = await User.create({ name, email, password, role });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
