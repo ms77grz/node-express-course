5e9ae9367f6e5304f9111f34fcb5728871c85381
ms77grz <ms77grz@gmail.com>
2021-10-25 15:38:45 +0300

    Auth Routes Structure

--- e-commerce-api/app.js
+++ e-commerce-api/app.js
@@ -1,10 +1,32 @@
+require('dotenv').config();
+require('express-async-errors');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-const port = process.env.PORT || 5000;
+const connectDB = require('./db/connect');
 
+const authRouter = require('./routes/authRoutes');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(morgan('tiny'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('e-commerce api');
+});
+
+app.use('/api/v1/auth', authRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening on port ${port}`));
   } catch (err) {
     console.log(err);
