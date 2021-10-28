e4781e637e076fd4acba853d640b3a14ef7d47f0
ms77grz <ms77grz@gmail.com>
2021-10-28 12:54:30 +0300

    Authorize Permissions - Setup

--- e-commerce-api/middleware/authentication.js
+++ e-commerce-api/middleware/authentication.js
@@ -4,13 +4,27 @@ const { isTokenValid } = require('../utils');
 const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token;
   if (!token) {
-    console.log('error, no token present');
-  } else {
-    console.log('token present');
+    throw new CustomError.UnauthenticatedError('Authentication Invalid');
+  }
+  try {
+    const { name, userId, role } = isTokenValid({ token });
+    req.user = { name, userId, role };
+    next();
+  } catch (err) {
+    throw new CustomError.UnauthenticatedError('Authentication Invalid');
+  }
+};
+
+const authorizePermissions = (req, res, next) => {
+  if (req.user.role !== 'admin') {
+    throw new CustomError.UnauthorizedError(
+      'Unauthorized to access this route'
+    );
   }
   next();
 };
 
 module.exports = {
   authenticateUser,
+  authorizePermissions,
 };
