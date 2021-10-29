7418fa8ce2c569104f8964d44b67e536202f900f
ms77grz <ms77grz@gmail.com>
2021-10-29 09:01:29 +0300

    UpdateUser Controller - FindOneAndUpdate()

--- e-commerce-api/controllers/userController.js
+++ e-commerce-api/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+const { attachCookiesToResponse, createTokenUser } = require('../utils');
 
 const getAllUsers = async (req, res) => {
   console.log(req.user);
@@ -21,7 +22,18 @@ const showCurrentUser = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-  res.send('updateUser');
+  const { name, email } = req.body;
+  if (!name || !email) {
+    throw new CustomError.BadRequestError('Please provide all values');
+  }
+  const user = await User.findOneAndUpdate(
+    { _id: req.user.userId },
+    { name, email },
+    { new: true, runValidators: true }
+  );
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 
 const updateUserPassword = async (req, res) => {
